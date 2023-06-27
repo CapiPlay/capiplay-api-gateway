86f5f533eb50cec960ee9f62678d981b5b175c16
henrique-al <henrique.a.lazzarino@gmail.com>
2023-06-27 19:23:35 -0300

    Initial commit

---
+++ src/main/java/br/senai/sc/capiplayapigateway/CapiplayApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sc.capiplayapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapiplayApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapiplayApiGatewayApplication.class, args);
+	}
+
+}
