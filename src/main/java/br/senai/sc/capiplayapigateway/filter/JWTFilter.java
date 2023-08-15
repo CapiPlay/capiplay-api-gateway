b6f84f2d8244839ec0329bf14e395991a9dacca9
Kaue Rafael Volles Freitas Teixeira <krafael@weg.net>
2023-08-15 20:33:08 -0300

    Anonimo anonimo anonimo

--- src/main/java/br/senai/sc/capiplayapigateway/filter/JWTFilter.java
+++ src/main/java/br/senai/sc/capiplayapigateway/filter/JWTFilter.java
@@ -1,6 +1,8 @@
 package br.senai.sc.capiplayapigateway.filter;
 
 import br.senai.sc.capiplayapigateway.utils.TokenService;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -28,18 +30,25 @@ public class JWTFilter implements GatewayFilter, Ordered {
 
     private final TokenService tokenService = new TokenService();
 
-    private final HashMap<String, HttpMethod> rotasPublicas = new HashMap<>();
-    private final AntPathMatcher pathMatcher = new AntPathMatcher();
+    private final List<Rota> autorizadas = new ArrayList<>();
+    private final List<Rota> indefinidas = new ArrayList<>();
 
-
+    private record Rota(String path, HttpMethod method) {
+        boolean match(String path, HttpMethod method) {
+            return this.method.equals(method) && new AntPathMatcher().match(this.path, path);
+        }
+    }
 
     public JWTFilter() {
-        rotasPublicas.put("/api/usuario/login", HttpMethod.POST);
-        rotasPublicas.put("/api/usuario/cadastro", HttpMethod.POST);
-        rotasPublicas.put("/api/video/buscar-completo/**", HttpMethod.GET);
-        rotasPublicas.put("/api/usuario/static/**", HttpMethod.GET);
-        rotasPublicas.put("/api/video/static/**", HttpMethod.GET);
-        rotasPublicas.put("/api/video/buscar-reels", HttpMethod.GET);
+        indefinidas.add(new Rota("/api/usuario/login", HttpMethod.POST));
+        indefinidas.add(new Rota("/api/usuario/cadastro", HttpMethod.POST));
+        indefinidas.add(new Rota("/api/video/buscar-completo/**", HttpMethod.GET));
+        indefinidas.add(new Rota("/api/usuario/static/**", HttpMethod.GET));
+        indefinidas.add(new Rota("/api/video/static/**", HttpMethod.GET));
+        indefinidas.add(new Rota("/api/video/buscar-reels", HttpMethod.GET));
+        indefinidas.add(new Rota("/api/usuario/anonimo", HttpMethod.POST));
+
+        autorizadas.add(new Rota("/api/usuario", HttpMethod.GET));
     }
 
     @Override
@@ -50,30 +59,41 @@ public class JWTFilter implements GatewayFilter, Ordered {
         String requestPath = request.getPath().toString();
         HttpMethod requestMethod = request.getMethod();
 
-        boolean isPublicRoute = rotasPublicas.entrySet().stream()
-                .anyMatch(entry -> pathMatcher.match(entry.getKey(), requestPath) &&
-                        entry.getValue().equals(requestMethod));
+        boolean isIndefinida = indefinidas.stream()
+                .anyMatch(rota -> rota.match(requestPath, requestMethod));
 
-        if (isPublicRoute) {
+        if (isIndefinida) {
             return chain.filter(exchange);
         }
-      
 
         var authHeader = request.getHeaders().getFirst("Authorization");
+
         if (authHeader == null) {
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
             return response.setComplete();
         }
         String token = authHeader.replace("Bearer ", "");
-        if (tokenService.validToken(token)) {
-            ServerHttpRequest newRequest = request.mutate()
-                    .header("usuarioId", tokenService.getId(token))
-                    .build();
 
-            return chain.filter(exchange.mutate().request(newRequest).build());
+        if (!tokenService.validToken(token)) {
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
         }
-        response.setStatusCode(HttpStatus.UNAUTHORIZED);
-        return response.setComplete();
+
+        DecodedJWT decodedJWT = JWT.decode(token);
+        boolean anonimo = decodedJWT.getClaim("anonimo").asBoolean();
+
+        boolean autorizado = autorizadas.stream()
+                .anyMatch(rota -> rota.match(requestPath, requestMethod)); ;
+
+        if (anonimo && autorizado) {
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+        ServerHttpRequest newRequest = request.mutate()
+                .header("usuarioId", tokenService.getId(token))
+                .build();
+
+        return chain.filter(exchange.mutate().request(newRequest).build());
     }
 
     @Override
